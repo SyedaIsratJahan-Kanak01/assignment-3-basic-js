00ad4b08a417d473ce34674f398a4e0b99e433fa
SyedaIsratJahan-Kanak01 <syeda.kanak@gmail.com>
2020-07-16 23:58:33 +0600

    solution two done

--- assignment.js
+++ assignment.js
@@ -7,3 +7,13 @@ function feetToMile(feet){
 
 var result = feetToMile(15840);
 console.log (result)
+
+/** Solution 02 */
+
+function woodCalculator(chairWood, tableWood, bedWood){
+    var totalWood = chairWood*1 + tableWood*3 + bedWood*5;
+    return totalWood;
+}
+
+var sumWood = woodCalculator(9, 5, 7);
+console.log(sumWood)
